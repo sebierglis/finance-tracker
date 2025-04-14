976f74d8ab1c619de7de58dc6db150f73469f123
sebierglis <sebastianerglis@gmail.com>
2025-04-14 10:46:08 -0300

    ✔️ Registro e inicio de sesión funcionando

--- docs/firebase.js
+++ docs/firebase.js
@@ -1,3 +1,4 @@
+// docs/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 
